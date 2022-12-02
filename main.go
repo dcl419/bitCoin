ef196bdd89c9a506394fb2369badcd804476a02f
dcl <1124373818@qq.com>
2022-12-02 22:34:45 +0800

    setHash

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"crypto/sha256"
+	"fmt"
+)
 
 // 1. 定义结构
 type Block struct {
@@ -16,14 +19,25 @@ type Block struct {
 func NewBlock(data string, preBlockHash []byte) *Block {
 	block := Block{
 		PreHash: preBlockHash,
-		Hash:    []byte{}, // 先填空, 后面再计算 //TODO
+		Hash:    []byte{},
 		Data:    []byte(data),
 	}
 
+	block.SetHash()
 	return &block
 }
 
 //6. 生成哈希
+func (block *Block) SetHash() {
+	// 1. 拼装数据
+	// block.Data...  将block.Data打散成byte
+	blockInfo := append(block.PreHash, block.Data...)
+	// 2. sha256
+	hash := sha256.Sum256(blockInfo)
+
+	block.Hash = hash[:]
+}
+
 //7. 引入区块链
 //8. 添加区块
 //9. 重构代码
